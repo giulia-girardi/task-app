d1d1192cb31b090c04134b96f66f8f082e9f839c
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-03 14:12:46 +0100

    created POST for signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const app = require('../app')
+const User = require('../models/User.model')
+const bcrypt = require('bcryptjs')
 
 //Get signup page
 
@@ -8,7 +10,25 @@ router.get('/signup', (req, res) => {
     res.render('authorization/signup')
 })
 
-//Get login page
+//Post sign up data
+router.post('/signup', async (req, res) => {
+    try {
+        const { firstname, lastname, email, password } = req.body;
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        await User.create({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: hashedPassword,
+        })
+        res.redirect('/profile')
+    } catch (error) {
+        res.render('/signup', { errorMessage: error.message })
+    }
+})
+
 
 // Get Login page
 router.get('/login', (req, res) => {
