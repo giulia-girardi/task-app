340cf980fac660f32eed50369d5f534e5b8f9697
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-03 16:16:52 +0100

    add post route for create

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const TaskModel = require('../models/Task.model');
 const router = express.Router();
 
 /* GET Dashboard page */
@@ -17,9 +18,21 @@ router.get("/create", (req, res, next) => {
     res.render("create-task");
 });
 
-/* GET Create Task page */
-router.get("/create", (req, res, next) => {
-    res.render("create-task");
+/* POST Create Task page */
+router.post("/create", async (req, res, next) => {
+    try {
+        const {taskName, dueDate, collaborators, taskCompleted } = req.body
+        await TaskModel.create({
+            taskName: taskName,
+            dueDate: dueDate, 
+            collaborators: collaborators,
+            taskCompleted: taskCompleted
+        })
+        res.redirect("/");
+    }
+    catch(error) {
+        res.render('create', {errorMessage: error})
+    }
 });
 
 /* GET Edit Task page */
