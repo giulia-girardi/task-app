a8f8d588502144132e42107ea35d4972a9fadddc
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-03 15:39:12 +0100

    add get routes

---
+++ routes/tasks.routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET Dashboard page */
+router.get("/dashboard", (req, res, next) => {
+    res.render("dashboard");
+});
+
+/* GET Tasks page */
+router.get("/", (req, res, next) => {
+    res.render("tasks");
+  });
+
+/* GET Create Task page */
+router.get("/create", (req, res, next) => {
+    res.render("create-task");
+});
+
+/* GET Edit Task page */
+router.get("/edit", (req, res, next) => {
+    res.render("edit-task");
+});
+
+
+module.exports = router;
